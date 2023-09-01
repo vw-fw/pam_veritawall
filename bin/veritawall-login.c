1a7f31fbac40426840227fbb1032c8cabd4358ac
softshield01 <devops@softshield.onmicrosoft.com>
2023-09-01 11:39:19 +0530

    m

---
+++ bin/veritawall-login.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2016 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <security/pam_appl.h>
+#include <security/openpam.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+static const char *service = "veritawall-login";
+static const char *user = "root";
+static int quiet = 0;
+
+static void
+usage(void)
+{
+	fprintf(stderr, "usage: man veritawall-login\n");
+	exit(EXIT_FAILURE);
+}
+
+int
+main(int argc, char **argv)
+{
+	pam_handle_t *pamh = NULL;
+	char pass[PASS_MAX + 1];
+	struct pam_conv pamc;
+	int pam_err, c;
+	int fd = -1;
+
+	while ((c = getopt(argc, argv, "h:qs:u:")) != -1) {
+		switch (c) {
+		case 'h': {
+			const char *errstr = NULL;
+
+			fd = strtonum(optarg, 0, INT_MAX, &errstr);
+			if (errstr) {
+				/* ignore faulty value */
+				fd = -1;
+			}
+			break;
+		}
+		case 'q':
+			quiet = 1;
+			break;
+		case 's':
+			service = optarg;
+			break;
+		case 'u':
+			user = optarg;
+			break;
+		default:
+			usage();
+			/* NOTREACHED */
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	if (argc) {
+		usage();
+		/* NOTREACHED */
+	}
+
+	if (fd != -1) {
+		char *p;
+		int b;
+
+		b = read(fd, pass, sizeof(pass) - 1);
+		if (b < 0) {
+			if (!quiet) {
+				fprintf(stderr, "File descriptor read "
+				    "failed.\n");
+			}
+			exit(EXIT_FAILURE);
+		}
+
+		pass[b] = '\0';
+
+		if ((p = strpbrk(pass, "\r\n")) != NULL) {
+			*p = '\0';
+		}
+	}
+
+	memset(&pamc, 0, sizeof(pamc));
+	pamc.conv = &openpam_ttyconv;
+
+	pam_err = pam_start(service, user, &pamc, &pamh);
+	if (pam_err == PAM_SUCCESS) {
+		if (fd != -1) {
+			/* this could fail and falls through to interactive */
+			pam_set_item(pamh, PAM_AUTHTOK, pass);
+		}
+		pam_err = pam_authenticate(pamh, 0);
+		if (pam_err == PAM_SUCCESS) {
+			pam_err = pam_acct_mgmt(pamh, 0);
+			if (pam_err == PAM_SUCCESS && !quiet) {
+				fprintf(stderr, "User %s successfully "
+				    "authenticated for service %s\n",
+				    user, service);
+			}
+		}
+	}
+
+	if (pam_err != PAM_SUCCESS && !quiet) {
+		fprintf(stderr, "User %s NOT authenticated for service %s\n",
+		    user, service);
+	}
+
+	if (pam_end(pamh, pam_err) != PAM_SUCCESS) {
+		exit(EXIT_FAILURE);
+	}
+
+	return (pam_err != PAM_SUCCESS);
+}
---
+++ lib/pam_veritawall.c
@@ -0,0 +1,152 @@
+/*
+ * Copyright (C) 2016-2019 Deciso B.V.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <syslog.h>
+
+
+static const char *auth_cmd = "/usr/local/libexec/veritawall-pam";
+static const char *auth_ret = "veritawall_session_return";
+
+struct veritawall_session {
+	int auth_status;
+};
+
+/* wipe status data */
+static void
+pam_veritawall_session_free(pam_handle_t *pamh, void *data, int pam_err)
+{
+	(void)pam_err;
+	(void)pamh;
+
+	free(data);
+}
+
+PAM_EXTERN int
+pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int
+pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+	const struct veritawall_session *session;
+	const void *item = NULL;
+	int pam_err;
+
+	pam_err = pam_get_data(pamh, auth_ret, &item);
+	if (pam_err != PAM_SUCCESS) {
+		return PAM_USER_UNKNOWN;
+	}
+
+	if (!item) {
+		return PAM_BUF_ERR;
+	}
+
+	session = item;
+
+	return session->auth_status;
+}
+
+PAM_EXTERN int
+pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int
+pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int
+pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+	return PAM_SERVICE_ERR;
+}
+
+PAM_EXTERN int
+pam_sm_authenticate(pam_handle_t *pamh, int flags,int argc, const char **argv)
+{
+	struct veritawall_session *session;
+	int script_response = 255;
+	const char *user;
+	char *password;
+	char *service;
+	int pam_err;
+	FILE *fp;
+
+	pam_err = pam_get_user(pamh, &user, NULL);
+	if (pam_err == PAM_SUCCESS) {
+		pam_err = pam_get_item(pamh, PAM_SERVICE, (const void **)&service);
+		if (pam_err == PAM_SUCCESS) {
+			pam_err = pam_get_authtok(pamh, PAM_AUTHTOK, (const char **)&password, NULL);
+			if (pam_err == PAM_SUCCESS) {
+				fp = popen(auth_cmd, "w");
+				if (!fp) {
+					pam_err = PAM_SYSTEM_ERR;
+				} else {
+					/* send authentication data to script */
+					fprintf(fp, "service=%s%c", service, 0);
+					fprintf(fp, "user=%s%c", user, 0);
+					fprintf(fp, "password=%s%c", password, 0);
+					/* extra NUL to mark end of data */
+					fprintf(fp, "%c", 0);
+
+					/* use exit status to authenticate */
+					script_response = pclose(fp);
+					if (script_response) {
+						if (WEXITSTATUS(script_response) == 2) {
+							// signal user unknown, so PAM may consider other options
+							pam_err = PAM_USER_UNKNOWN;
+						} else {
+							pam_err = PAM_AUTH_ERR;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	session = malloc(sizeof(*session));
+	if (!session) {
+		return PAM_BUF_ERR;
+	}
+
+	session->auth_status = pam_err;
+
+	pam_set_data(pamh, auth_ret, session, pam_veritawall_session_free);
+	return (pam_err);
+}
